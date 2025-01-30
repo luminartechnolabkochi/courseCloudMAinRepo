d3a427b72becc83ebda863f3abd3b1a6ddab3be1
sajay <sajaykannan10@gmail.com>
2025-01-30 11:54:34 +0530

    created student app

--- instructor/admin.py
+++ instructor/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin import sites
-from instructor.models import User, Category, Course, Module, Lesson
+from instructor.models import User, Category, Course, Module, Lesson,InstructorProfile
 
 
 
@@ -34,5 +34,6 @@ class ModuleAdmin(admin.ModelAdmin):
 admin.site.register(Module, ModuleAdmin)
 
 
+admin.site.register(InstructorProfile)
 
 
---
+++ student/forms.py
@@ -0,0 +1,20 @@
+
+
+from django import forms
+
+
+from instructor.models import User
+
+from django.contrib.auth.forms import UserCreationForm
+
+
+class StudentCreateForm(UserCreationForm):
+
+    class Meta:
+
+        model=User
+
+        fields=["username","password1","password2","email"]
+
+
+
